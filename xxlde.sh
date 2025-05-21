d0ea227ca447edd98f908c15bfc02923d9e44e2b
ymailo <universitasindo.id000044@gmail.com>
2025-05-21 14:33:21 +0700

    Update xxlde.sh

--- xxlde.sh
+++ xxlde.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/3F49Xu1 > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/ymailo/my4/refs/heads/main/3389 > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
