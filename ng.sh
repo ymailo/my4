4892ccd8e2423ce2c3a899a66b2ddec6846fde92
ymailo <universitasindo.id000044@gmail.com>
2025-05-22 16:52:58 +0000

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -22,7 +22,7 @@ echo "===================80%"
 sudo service xrdp start > /dev/null 2>&1
 echo "=======================100%"
 echo XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://127.0.0.1:22/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
 echo "Don't close tab colab to keep rdp running 12 hours"
 echo "👉Keep support akuh.net thank you👈"
